9a16bdd688be2857c88b8eafd05cb4ee79ebfd3a
brahim <brahimidmansour015@gmail.com>
2022-08-20 16:26:36 +0100

    config changes

--- src/main/java/com/stage/crudApi/controller/UserController.java
+++ src/main/java/com/stage/crudApi/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,6 +18,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1")
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableWebSecurity
 public class UserController {
     @Autowired
     UserService userService;
